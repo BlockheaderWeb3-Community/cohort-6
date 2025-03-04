3bb9c101609301b410ae825350c8fea04da874d6
gordons <godwinobas0@gmail.com>
2025-03-04 07:05:50 +0100

    implemented tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-ignition-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.25",
+  solidity: "0.8.28",
 };
